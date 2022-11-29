1773d9f5791ed7fc34566a3fe97c72ff3d6cb205
Norbert Domagala <norbert.domagala2005@gmail.com>
2022-11-29 23:38:17 +0100

    Aded function who is changing champion id to name and aded function who changing player every rune ids to names

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ DATAS = {
 
 
 class Summoner:
-    def __init__(self, summoner_info_from_request):
+    def __init__(self, summoner_info_from_request: requests.Response):
         self._summoner_info_from_request = summoner_info_from_request
 
-    def summoner_info(self):
+    def summoner_info(self) -> dict | None:
         if check_for_errors(self._summoner_info_from_request):
             return self._summoner_info_from_request.json()
 
@@ -28,10 +28,10 @@ class Summoner:
 
 
 class Game:
-    def __init__(self, game_info_from_request):
+    def __init__(self, game_info_from_request: requests.Response):
         self._game_info_from_request = game_info_from_request
 
-    def current_game_info(self):
+    def current_game_info(self) -> dict | None:
         if check_for_errors(self._game_info_from_request):
             return self._game_info_from_request.json()
 
@@ -39,11 +39,7 @@ class Game:
         return None
 
 
-def get_info_from_request(api_info, api_key, info_to_request):
-    return requests.get(url=f"{info_to_request}{api_info}", params={"api_key": api_key})
-
-
-def check_for_errors(request):
+def check_for_errors(request: requests.Response) -> bool:
     if request.ok:
         return True
 
@@ -60,7 +56,16 @@ def check_for_errors(request):
         sys.exit(1)
 
 
-def game_summoners_info(current_game_info: dict) -> list[tuple[any, any, any]]:
+def get_info_from_request(api_info: str, info_to_request: str, api_key: str) -> requests.Response:
+    return requests.get(url=f"{api_info}{info_to_request}", params={"api_key": api_key})
+
+
+def get_api_key() -> str:
+    with open("my_api_key.txt", "r", encoding="utf-8") as file_with_api_key:
+        return file_with_api_key.read()
+
+
+def isolation_the_summoners_game_info(current_game_info: dict) -> list[tuple[any, any, any]]:
     summoner_names = (element["summonerName"] for element in current_game_info["participants"])
     champions = (element["championId"] for element in current_game_info["participants"])
     runes = (element["perks"]["perkIds"] for element in current_game_info["participants"])
@@ -68,16 +73,21 @@ def game_summoners_info(current_game_info: dict) -> list[tuple[any, any, any]]:
     return list(zip(summoner_names, champions, runes))
 
 
-def get_api_key():
-    with open("api_key", "r", encoding="utf-8") as file:
-        return file.read()
+def changing_champion_id_to_name(champion_id: str, url_to_data: str) -> str:
+    url_data = f"{url_to_data}{champion_id}.json"
+    return requests.get(url_data).json()["name"]
 
 
-def check_player_current_game(summoner_name):
+def changing_player_rune_ids_to_names(rune_ids: list[int], url_to_data: str) -> tuple[str]:
+    data = requests.get(url_to_data).json()
+    return tuple(''.join((section["name"] for section in data if section["id"] == rune_id)) for rune_id in rune_ids)
+
+
+def check_player_current_game(summoner_name: str):
     api_key = get_api_key()
 
     summoner_info_from_request = get_info_from_request(
-        summoner_name, api_key, API_ENDPOINTS["get_summoner_info"]
+        API_ENDPOINTS["get_summoner_info"], summoner_name, api_key
     )
 
     summoner = Summoner(summoner_info_from_request)
@@ -87,14 +97,17 @@ def check_player_current_game(summoner_name):
         return None
 
     current_game_info = get_info_from_request(
-        summoner_info["id"], api_key, API_ENDPOINTS["get_current_game_info"]
+        API_ENDPOINTS["get_current_game_info"], summoner_info["id"], api_key
     )
 
     current_game = Game(current_game_info)
     game_info = current_game.current_game_info()
 
-    return None if game_info is None else game_summoners_info(game_info)
+    return None if game_info is None else isolation_the_summoners_game_info(game_info)
 
 
 if __name__ == "__main__":
-    check_player_current_game("")
+    print(check_player_current_game(""))
+    # print(changing_player_rune_ids_to_names(
+    #     [8128, 8139, 8138, 8106, 8313, 8321, 5008, 5008, 5002], DATAS["get_runes_data"])
+    # )
