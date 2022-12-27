802a5daed62fc96680d18030c57b21697bfc14ed
Norbert Domagala <norbert.domagala2005@gmail.com>
2022-12-27 20:35:01 +0100

    Code simplification

--- main.py
+++ main.py
@@ -1,40 +1,39 @@
-import sys
-import requests
 from os import getenv
 from dotenv import load_dotenv
+import requests
 from references import API_ENDPOINTS, GAME_DATA
-from operations_on_data import (
-    isolation_the_summoners_info,
-    changing_champion_id_to_name,
-    changing_player_rune_ids_to_names,
-)
+import operations_on_data
 
 
-class Summoner:
-    def __init__(self, summoner_info_from_request: requests.Response):
-        self._summoner_info_from_request = summoner_info_from_request
+def get_summoner_info(summoner_info_endpoint, summoner_name, api_key) -> dict | None:
+    summoner_info_from_request = get_info_from_request(
+        summoner_info_endpoint, summoner_name, api_key
+    )
 
-    def summoner_info(self) -> dict | None:
-        if check_for_errors(self._summoner_info_from_request):
-            return self._summoner_info_from_request.json()
+    if check_summoner_info_request := check_for_errors(summoner_info_from_request):
+        return summoner_info_from_request.json()
 
+    elif not check_summoner_info_request:
         print("Summoner with this username does not exist")
-        return None
+
+    return None
 
 
-class Game:
-    def __init__(self, game_info_from_request: requests.Response):
-        self._game_info_from_request = game_info_from_request
+def get_current_game_info(current_game_info_endpoint, summoner_id, api_key) -> dict | None:
+    game_info_from_request = get_info_from_request(
+        current_game_info_endpoint, summoner_id, api_key
+    )
 
-    def current_game_info(self) -> dict | None:
-        if check_for_errors(self._game_info_from_request):
-            return self._game_info_from_request.json()
+    if check_current_game_request := check_for_errors(game_info_from_request):
+        return game_info_from_request.json()
 
+    elif not check_current_game_request:
         print("The summoner is not currently in the game")
-        return None
+
+    return None
 
 
-def check_for_errors(request: requests.Response) -> bool:
+def check_for_errors(request: requests.Response) -> bool | None:
     if request.ok:
         return True
 
@@ -42,35 +41,28 @@ def check_for_errors(request: requests.Response) -> bool:
         request.raise_for_status()
 
     except requests.exceptions.RequestException as request_error:
-        NO_ACCESS_CODE = "404"
-        current_error_code = str(request_error)[:3]
+        NO_ACCESS_CODE = 404
+        current_error_code = request_error.response.status_code
 
         if current_error_code == NO_ACCESS_CODE:
             return False
 
-        print(f"Request Error: {request_error}")
-        sys.exit(1)
+        print(
+            f"Error code: {current_error_code}\n"
+            f"Error message: {request_error.response.reason}"
+        )
+        return None
 
 
 def get_info_from_request(
-    api_info: str, info_to_request: str, api_key: str
+    api_info: str,
+    info_to_request: str,
+    api_key: str,
 ) -> requests.Response:
     return requests.get(url=f"{api_info}{info_to_request}", params={"api_key": api_key})
 
 
-def get_changed_summoner_info_in_dict(
-    champion_id: str,
-    rune_ids: list[int],
-    url_to_champions_data: str,
-    url_to_runes_data: str,
-) -> dict[str, tuple[str]]:
-    return {
-        "champion_name": changing_champion_id_to_name(champion_id, url_to_champions_data),
-        "rune_names": changing_player_rune_ids_to_names(rune_ids, url_to_runes_data),
-    }
-
-
-def dict_with_every_summoner_changed_info(
+def changed_current_game_summoners_info(
     isolated_summoners_info: list[tuple[any, any, any]],
     url_to_champions_data: str,
     url_to_runes_data: str,
@@ -82,42 +74,43 @@ def dict_with_every_summoner_changed_info(
         champion_id = summoner[1]
         rune_ids = summoner[2]
 
-        final_summoners_info[summoner_name] = get_changed_summoner_info_in_dict(
-            champion_id, rune_ids, url_to_champions_data, url_to_runes_data
-        )
+        final_summoners_info[summoner_name] = {
+            "champion_name": operations_on_data.change_champion_id_to_champion_name(
+                champion_id, url_to_champions_data
+            ),
+            "rune_names": operations_on_data.change_player_rune_ids_to_rune_names(
+                rune_ids, url_to_runes_data
+            ),
+        }
 
     return final_summoners_info
 
 
 def check_the_player_current_game(summoner_name: str) -> dict[str, dict[str, tuple[str]]] | None:
-    summoner_info_from_request = get_info_from_request(
+    summoner_info = get_summoner_info(
         API_ENDPOINTS["get_summoner_info"], summoner_name, getenv("API_KEY")
     )
-
-    summoner = Summoner(summoner_info_from_request)
-    summoner_info = summoner.summoner_info()
 
     if summoner_info is None:
         return None
 
-    current_game_info = get_info_from_request(
+    current_game_info = get_current_game_info(
         API_ENDPOINTS["get_current_game_info"], summoner_info["id"], getenv("API_KEY")
     )
 
-    current_game = Game(current_game_info)
-    game_info = current_game.current_game_info()
-
-    if game_info is None:
+    if current_game_info is None:
         return None
 
-    isolated_summoners_info = isolation_the_summoners_info(game_info)
+    isolated_summoners_info = operations_on_data.isolate_needed_summoners_info(current_game_info)
     url_to_champions_data = GAME_DATA["get_champions_data"]
     url_to_runes_data = GAME_DATA["get_runes_data"]
-    return dict_with_every_summoner_changed_info(
+
+    return changed_current_game_summoners_info(
         isolated_summoners_info, url_to_champions_data, url_to_runes_data
     )
 
 
 if __name__ == "__main__":
     load_dotenv()
-    print(check_the_player_current_game(""))
+    name_of_the_summoner = input("Enter the summoner's name -> ")
+    print(check_the_player_current_game(name_of_the_summoner))
